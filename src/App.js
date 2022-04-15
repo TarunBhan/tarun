0274f158e4adef61025490d9369d9a13b1aca1f5
Tarun <tarunbhanthakur@gmail.com>
2022-04-16 00:05:19 +0530

    test app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import Header  from './components/Header';
+import Header from './components/Header';
 import './App.css';
 import React from 'react';
 function App() {
   return (
-  <div className="App">
-    <Header />
-  </div>
+    <div className="App">
+      <h1> new data added </h1>
+      <Header />
+    </div>
   );
 }
 
